967799e249fdace68ce054c08d21c456151f41e7
Audrey-Ella-xo <anitaudrey@gmail.com>
2020-08-04 11:47:19 +0100

    Add weather API with keys and promise

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ const header = () => {
   navBar.innerHTML = `
         <nav class="navbar navbar-light bg-light">
         <a class="navbar-brand">Ella's Weather App</a>
-        <form class="form-inline">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <form class="form-inline change-location">
+            <input class="form-control mr-sm-2" type="search" name="city" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
         </nav>
--- src/weather.js
+++ src/weather.js
@@ -1,5 +1,17 @@
 const weatherPromise = () => {
   const key = '3d86e91bf2b41fc3fd1f768f864ed86c';
+  const submit = document.querySelector('button');
+  const cityForm = document.querySelector('form');
+
+  const weatherCard = document.createElement('div');
+  weatherCard.setAttribute('class', 'card');
+  weatherCard.classList.add('shadow-lg', 'rounded', 'card-container', 'my-5', 'mx-auto');
+
+  const weatherDetails = document.createElement('div');
+  weatherDetails.setAttribute('class', 'card');
+  weatherDetails.classList.add('text-muted', 'text-uppercase', 'text-center');
+
+  weatherCard.appendChild(weatherDetails);
 
   const getWeather = async (city) => {
     const base = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
@@ -10,10 +22,52 @@ const weatherPromise = () => {
     return data;
   };
 
-  getWeather('london')
-    .then(data => console.log(data))
+  const updateUI = (data) => {
+    // const { weatherDet } = data;
+    const cityName = data.name;
+    const { country } = data.sys;
+    const tempValue = data.main.temp;
+    // const tempValue = Math.floor(data.main.temp - KELVIN);
+    const { description } = data.weather[0];
+    const iconId = data.weather[0].icon;
+
+    // update details template;
+    weatherDetails.innerHTML = `
+      <h5 class="my-3">${cityName}, ${country}</h5>
+      <div class="my-3">${description}</div>
+      <div class="display-4 my-4">
+        <span>${tempValue}</span>
+        <span>&deg;C</span>
+      </div>
+    `;
+  };
+
+  submit.addEventListener('click', e => {
+    e.preventDefault();
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    const updateCity = async (city) => {
+      const weatherDet = await getWeather(city);
+
+      return weatherDet;
+    };
+
+    // Update UI with city value;
+    updateCity(city)
+      .then(data => updateUI(data))
+      .catch(err => console.log(err));
+  });
+
+  getWeather('lagos')
+    .then(data => {
+      console.log(data);
+      console.log(data.name);
+      console.log(data.sys.country);
+    })
     .catch(err => console.log(err));
-//   return getWeather();
+  //   return getWeather();
+  document.body.appendChild(weatherCard);
 };
 
 export default weatherPromise;
