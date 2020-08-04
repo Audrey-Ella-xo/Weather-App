298cfca1c4cc4ffb8b70219f32a98218a67644ac
Audrey-Ella-xo <anitaudrey@gmail.com>
2020-08-04 11:48:49 +0100

    Add dom elements to weather.js

--- src/weather.js
+++ src/weather.js
@@ -1,6 +1,6 @@
 const weatherPromise = () => {
   const key = '3d86e91bf2b41fc3fd1f768f864ed86c';
-  const submit = document.querySelector('button');
+  
   const cityForm = document.querySelector('form');
 
   const weatherCard = document.createElement('div');
@@ -22,43 +22,7 @@ const weatherPromise = () => {
     return data;
   };
 
-  const updateUI = (data) => {
-    // const { weatherDet } = data;
-    const cityName = data.name;
-    const { country } = data.sys;
-    const tempValue = data.main.temp;
-    // const tempValue = Math.floor(data.main.temp - KELVIN);
-    const { description } = data.weather[0];
-    const iconId = data.weather[0].icon;
-
-    // update details template;
-    weatherDetails.innerHTML = `
-      <h5 class="my-3">${cityName}, ${country}</h5>
-      <div class="my-3">${description}</div>
-      <div class="display-4 my-4">
-        <span>${tempValue}</span>
-        <span>&deg;C</span>
-      </div>
-    `;
-  };
-
-  submit.addEventListener('click', e => {
-    e.preventDefault();
-    const city = cityForm.city.value.trim();
-    cityForm.reset();
-
-    const updateCity = async (city) => {
-      const weatherDet = await getWeather(city);
-
-      return weatherDet;
-    };
-
-    // Update UI with city value;
-    updateCity(city)
-      .then(data => updateUI(data))
-      .catch(err => console.log(err));
-  });
-
+  
   getWeather('lagos')
     .then(data => {
       console.log(data);
