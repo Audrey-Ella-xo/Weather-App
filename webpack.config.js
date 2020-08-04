075689e0afbbe8a976951ba0814d72af61aab706
Audrey-Ella-xo <anitaudrey@gmail.com>
2020-08-04 07:16:57 +0100

    Add webpack configurations, build and setup

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+// generate an index.html file
+// eslint-disable-next-line import/no-unresolved
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// clean the dist folder before each build
+// eslint-disable-next-line import/no-unresolved
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Weather App',
+    }),
+  ],
+};
